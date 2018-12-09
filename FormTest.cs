5c00f28b03700b9bb2f61cf07bf29393995c19e0
miwa <suconbu@LIVE.JP>
2018-12-09 21:55:52 +0900

    テストコード更新

--- FormTest.cs
+++ FormTest.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace BoxLayouting
 {
@@ -29,28 +31,69 @@ namespace BoxLayouting
         {
             base.OnLoad(e);
 
-            var a = view.Add(".a");
-            a.Data = new Pen(Color.Red, 1.0f);
-            a.SetPosition("20vmin");
+            try
+            {
+                var a = view.Add("a");
+                a.Data = new Pen(Color.Red, 1.0f);
+                a.SetPosition("50px");
 
-            var aa = a.Add(".a.a");
-            aa.Data = new Pen(Color.Orange, 1.0f);
-            aa.SetSize("100px", "50px");
+                var aa = a.Add("a.a");
+                aa.Data = new Pen(Color.Orange, 1.0f);
+                aa.SetSize("50px", "50px");
 
-            var ab = a.Add(".a.b");
-            ab.Data = new Pen(Color.DarkGoldenrod, 1.0f); ;
-            ab.SetPosition(null, "5vw", null, null);
-            ab.SetSize("200px", "50px");
-            ab.SetCenter(null, "80%");
+                var ab = a.Add("a.b");
+                ab.Data = new Pen(Color.Orange, 1.0f);
+                ab.SetSize("50px", "50px");
+                ab.SetPosition("50px", "50px", null, null);
 
-            var b = view.Add(".b");
-            b.Data = new Pen(Color.Blue, 1.0f); ;
-            b.SetCenter("70%", "50%");
-            b.SetSize("50%", "10vh");
+                var ac = a.Add("a.c");
+                ac.Data = new Pen(Color.Orange, 1.0f);
+                ac.SetSize("50px", "50px");
+                ac.SetPosition(null, null, "50px", "50px");
+
+                var ad = a.Add("a.d");
+                ad.Data = new Pen(Color.Orange, 1.0f);
+                ad.SetSize("50px", "50px");
+                // Size指定ありかつPosition両端指定の場合はLeft/Topの方を採用
+                ad.SetPosition("100px", "100px", "100px", "100px");
+
+                var ae = a.Add("a.e");
+                ae.Data = new Pen(Color.Orange, 1.0f);
+                ae.SetSize("50px", "50px");
+                // %は親要素のエリアに対する割合
+                ae.SetCenter("50%", "50%");
+                // Center指定ありの場合はPositionはすべて無視
+                ae.SetPosition("100px", "100px", "100px", "100px");
+
+                var b = view.Add("b");
+                b.Data = new Pen(Color.Blue, 1.0f);
+                b.SetPosition("20vh", "20vw");
+
+                var ba = b.Add("b.a");
+                ba.Data = new Pen(Color.RoyalBlue, 1.0f);
+                ba.SetSize("100px", "100px");
+                ba.SetPosition("-50px", null, null, "+50px");
+            }
+            catch (Exception ex)
+            {
+                ;
+            }
 
             this.view.Width = this.ClientRectangle.Width;
             this.view.Height = this.ClientRectangle.Height;
-            this.view.Recalculate();
+            var sw = Stopwatch.StartNew();
+            for (var i = 0; i < 1000; i++)
+            {
+                this.view.Recalculate();
+            }
+            Console.WriteLine($"Recalculate: {sw.ElapsedMilliseconds / 1000.0f}ms");
+
+            var sb = new StringBuilder();
+            view.Traverse(box =>
+            {
+                sb.AppendLine($"{box.Name}:{box.Bounds.ToString()}");
+            });
+            File.WriteAllText($"test_bounds_{DateTime.Now.Ticks}.txt", sb.ToString());
 
             this.Invalidate();
         }
